50c61f44bcb12bb46ced15dd107e0e59bb86f428
Jayesh Sharma <jslink@protonmail.com>
2023-03-09 22:13:43 +0530

    Apply basic click operations and ensure they are running as intended.(added click event listeners on rock, paper and scissor buttons respectively)

--- jsrpsui/scriptrpsuicheck.js
+++ jsrpsui/scriptrpsuicheck.js
@@ -4,8 +4,11 @@
 
 rockclickbutton = document.getElementById("rockweapon"); // clicking on rock button, image inside
 paperclickbutton = document.getElementById("paperweapon");
-scissorclickbutton = document.getElementById("paperweapon");
+scissorclickbutton = document.getElementById("scissorweapon");
+
 rockclickbutton.addEventListener("click", testFunc);
+paperclickbutton.addEventListener("click", testFunc2);
+scissorclickbutton.addEventListener("click", testFunc3);
 
 rockweapondisplay = document.getElementById("rockweaponimage"); //  rock image
 //document.getElementById("playeroneimage");
@@ -72,6 +75,110 @@ function testFunc() { // what happens on the click
       document.getElementById("cpuimage").src = "scissor.png";
       updatecpuscoreid.innerText = cpucounter + 0;
       updateplayerscoreid.innerText = pcounter + 1;
+      }
+
+    console.log(`CPU has chosen to call upon ${compVal}!\n`);
+    //document.getElementById("playeroneimage").src = "rock.png";
+    //document.getElementById("cpuscoreid").innerText = 1; // used innerHTML
+    //let updatecpuscoreid = document.getElementById("cpuscoreid");
+    //updatecpuscoreid.innerHTML += +5078;
+    //updatecpuscoreid.innerText = cpucounter + 1234;
+  // }
+}
+
+function testFunc2() {
+  let pSelect = "PAPER";
+
+    // add checking condition(s) here
+
+    // console.log(`Ye fine yeoman ${playerName1}, you have decided to go with ${pSelect.toUpperCase()}!\n`);
+    // console.log("Time for the CPU to decide on its weapon...\n");
+
+    let array = ["ROCK", "PAPER", "SCISSOR"];
+
+    let compSelect = function (array) {
+      return array[Math.floor(Math.random()*array.length)];
+    };
+
+    let compVal = compSelect(array);
+
+    if (pSelect.toUpperCase() === "PAPER" && compVal === "PAPER") {
+      // console.log(`Goddamnit! It's a tie! Score remains ${pCounter} - ${compCounter}. Care to tempt fate again?\n`);
+      document.getElementById("playeroneimage").src = "paper.png";
+      document.getElementById("cpuimage").src = "paper.png";
+      updatecpuscoreid.innerText = cpucounter + 0;
+      updateplayerscoreid.innerText = pcounter + 0;
+
+      }
+
+      else if (pSelect.toUpperCase() === "PAPER" && compVal === "ROCK") {
+      // compCounter = compCounter + 1;
+      // console.log(`${compCounter} - ${pCounter} to the CPU! PAPER engulfs ${playerName1}'s ROCK!\n`);
+      document.getElementById("playeroneimage").src = "paper.png";
+      document.getElementById("cpuimage").src = "rock.png";
+      updatecpuscoreid.innerText = cpucounter + 0;
+      updateplayerscoreid.innerText = pcounter + 1;
+      }
+
+      else if (pSelect.toUpperCase() === "PAPER" && compVal === "SCISSOR") {
+        // pCounter = pCounter + 1;
+        // console.log(`${pCounter} - ${compCounter} to ${playerName1}! ${playerName1}'s ROCK smashed CPU's SCISSOR!\n`);
+        document.getElementById("playeroneimage").src = "paper.png";
+      document.getElementById("cpuimage").src = "scissor.png";
+      updatecpuscoreid.innerText = cpucounter + 1;
+      updateplayerscoreid.innerText = pcounter + 0;
+      }
+
+    console.log(`CPU has chosen to call upon ${compVal}!\n`);
+    //document.getElementById("playeroneimage").src = "rock.png";
+    //document.getElementById("cpuscoreid").innerText = 1; // used innerHTML
+    //let updatecpuscoreid = document.getElementById("cpuscoreid");
+    //updatecpuscoreid.innerHTML += +5078;
+    //updatecpuscoreid.innerText = cpucounter + 1234;
+  // }
+}
+
+function testFunc3() {
+  let pSelect = "SCISSOR";
+
+    // add checking condition(s) here
+
+    // console.log(`Ye fine yeoman ${playerName1}, you have decided to go with ${pSelect.toUpperCase()}!\n`);
+    // console.log("Time for the CPU to decide on its weapon...\n");
+
+    let array = ["ROCK", "PAPER", "SCISSOR"];
+
+    let compSelect = function (array) {
+      return array[Math.floor(Math.random()*array.length)];
+    };
+
+    let compVal = compSelect(array);
+
+    if (pSelect.toUpperCase() === "SCISSOR" && compVal === "SCISSOR") {
+      // console.log(`Goddamnit! It's a tie! Score remains ${pCounter} - ${compCounter}. Care to tempt fate again?\n`);
+      document.getElementById("playeroneimage").src = "scissor.png";
+      document.getElementById("cpuimage").src = "scissor.png";
+      updatecpuscoreid.innerText = cpucounter + 0;
+      updateplayerscoreid.innerText = pcounter + 0;
+
+      }
+
+      else if (pSelect.toUpperCase() === "SCISSOR" && compVal === "PAPER") {
+      // compCounter = compCounter + 1;
+      // console.log(`${compCounter} - ${pCounter} to the CPU! PAPER engulfs ${playerName1}'s ROCK!\n`);
+      document.getElementById("playeroneimage").src = "scissor.png";
+      document.getElementById("cpuimage").src = "paper.png";
+      updatecpuscoreid.innerText = cpucounter + 0;
+      updateplayerscoreid.innerText = pcounter + 1;
+      }
+
+      else if (pSelect.toUpperCase() === "SCISSOR" && compVal === "ROCK") {
+        // pCounter = pCounter + 1;
+        // console.log(`${pCounter} - ${compCounter} to ${playerName1}! ${playerName1}'s ROCK smashed CPU's SCISSOR!\n`);
+        document.getElementById("playeroneimage").src = "scissor.png";
+      document.getElementById("cpuimage").src = "rock.png";
+      updatecpuscoreid.innerText = cpucounter + 1;
+      updateplayerscoreid.innerText = pcounter + 0;
       }
 
     console.log(`CPU has chosen to call upon ${compVal}!\n`);
